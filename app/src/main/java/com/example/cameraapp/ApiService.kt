72c3a0fdc85b8176d446f39f74b78847014cb451
new name <nandhakumar097@gmail.com>
2023-11-30 14:18:51 +0530

    Removed unused file

--- app/src/main/java/com/example/cameraapp/ApiService.kt
+++ app/src/main/java/com/example/cameraapp/ApiService.kt
@@ -1,8 +1,6 @@
 package com.example.cameraapp
 
-import okhttp3.Call
 import okhttp3.MultipartBody
-import okhttp3.RequestBody
 import retrofit2.http.Multipart
 import retrofit2.http.POST
 import retrofit2.http.Part
